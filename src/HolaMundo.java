f52da21dcfa33ac7a77aaf7a2f35b84e071ab94a
Rch777 <nuevoinformatico@gmail.com>
2018-10-01 01:25:48 -0500

    Mas cambios con visual Studio Code

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -4,6 +4,7 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 		System.out.println("Pruebas en Git and GitHub");
+		System.out.println("Practicando en Java con Visual Studio Code");
 		int a = 5;
 		int b = 6;
 		int c = a + b;
