2853c3675f32d3d85f3e829602f06c596738286f
Rch777 <nuevoinformatico@gmail.com>
2018-10-01 01:30:57 -0500

    Cambios en Eclipse

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -5,6 +5,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Pruebas en Git y GitHub");
+		System.out.println("Estos son cambios en Eclipse");
 		int a = 5;
 		int b = 6;
 		int c = a + b;
